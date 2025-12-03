04470f4b4ca1ecdf97f972f0b1232ad5b2f3c034
YouRun <kls6378@naver.com>
2025-12-03 18:16:21 +0900

    배포용 설정 추가

--- server.js
+++ server.js
@@ -18,6 +18,18 @@ app.use("/api", authRoutes);
 app.use("/api", scheduleRoutes);
 app.use("/api", groupRoutes);
 
+
+// 배포용 설정
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+// API 요청(/api/...)이 아닌 모든 요청은 리액트(index.html)로 보냄
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 // 서버 실행
 app.listen(PORT, () => {
   console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다!`);
